0d899f7899d8d1ab3281fcdf7d66b1433546037e
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-09-26 09:03:07 -0400

    minor fix on gitignore and eval.py

--- UCI_HAR_GANs/evaluate.py
+++ UCI_HAR_GANs/evaluate.py
@@ -73,7 +73,7 @@ def get_accuracy(model, data, labels, class_stats=False):
             # print out the class accuracies
             for i in range(len(classes)):
                 print(f"Class {classes[i]}:\t{class_acc[i]*100}%")
-    
+
     # print out the accuracy
     print(f"Accuracy of the model on testing set: {percent_correct*100}%")
 
@@ -98,3 +98,5 @@ def evaluate(true_data, generated_data, classifier_path):
     true_correct = get_accuracy(classifier, true_x, true_y.type(torch.int16), class_stats=True)
     print("\n---Classifier Performance On Fake Data---\n")
     generated_correct = get_accuracy(classifier, generated_x, generated_y.type(torch.int16), class_stats=True)
+
+    return true_correct, generated_correct
