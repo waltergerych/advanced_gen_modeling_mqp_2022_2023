4a31de9fc07aba69585e3bff9eac5e5a4edb9a26
jasondykstra <jasondykstra2000@gmail.com>
2023-02-27 10:24:31 -0500

    added preliminary code for classification model

--- diffusion_models/main.py
+++ diffusion_models/main.py
@@ -8,6 +8,17 @@ import torch.optim as optim
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.feature_selection import SelectKBest
+import torch
+import torch.nn as nn
+import torchvision
+import torch.nn.functional as F
+from torch.optim import Adam
+from torch.autograd import Variable
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import transforms
+from torch.utils.data import DataLoader
+import glob
+from PIL import Image
 
 from utils import * 
 from model import ConditionalModel
@@ -16,6 +27,7 @@ from evaluate import *
 from classifier import *
 from diffusion import *
 from gan import *
+
 
 # Set plot style
 hdr_plot_style()
@@ -38,7 +50,8 @@ test_x, test_y = load_data('dataset/UCI_HAR_Dataset', 'test')
     # print(f)
 # print(os.path.exists('../dataset/UCI_HAR_Dataset_Triaxial/train/Intertial_Signals'))
 total_acc_x_train = np.loadtxt('dataset/UCI_HAR_Dataset_Triaxial/train/Inertial_Signals/total_acc_x_train.txt')
-body_gyro_x_train = np.loadtxt('dataset/UCI_HAR_Dataset_Triaxial/train/Inertial_Signals/body_gyro_x_train.txt')
+total_acc_y_train = np.loadtxt('dataset/UCI_HAR_Dataset_Triaxial/train/Inertial_Signals/total_acc_y_train.txt')
+total_acc_z_train = np.loadtxt('dataset/UCI_HAR_Dataset_Triaxial/train/Inertial_Signals/total_acc_z_train.txt')
 CUSTOM_TYPE = "three_values"
 custom_acc = np.loadtxt(f'dataset/UCI_HAR_Dataset_Triaxial/train/Inertial_Signals/custom_acc_{CUSTOM_TYPE}.txt')
 
@@ -68,7 +81,7 @@ from pyts.image import GramianAngularField
 # Define dataset to generate images on (Must be 2D array)
 # Using only x axis of total acceleration to test
 # task = total_acc_x_train[0:1000]
-task = total_acc_x_train
+task = total_acc_z_train
 
 # Define GAF and fit to data
 gadf = GramianAngularField(image_size=48, method='difference')
@@ -77,25 +90,238 @@ X_gadf = gadf.fit_transform(task)
 len_task = len(task)
 
 # Check that the directory to save the images exists or create it
-GAF_DIRECTORY = 'diffusion_models/GAF_Class_Data'
+GAF_DIRECTORY = 'diffusion_models/GAF_RGB_Images'
+
 if not os.path.isdir(GAF_DIRECTORY):
     os.makedirs(GAF_DIRECTORY, exist_ok=True)
 
 labels_int = [int(x) for x in labels.numpy()]
 
 # Iterate over the trajectories to create the images
-for i in range(len_task):
-    print(f'\r{i}/{len_task} - {round(i/len_task*100, 2)}%', end='')
-    plt.axis('off')
-    plt.imshow(X_gadf[i], extent=[0, 1, 0, 1], cmap = 'coolwarm', aspect = 'auto',vmax=abs(X_gadf[i]).max(), vmin=-abs(X_gadf[i]).max())
-    plt.savefig(f'{GAF_DIRECTORY}/{HAR_data_classes[labels_int[i]]}/{i}.jpg', bbox_inches='tight')
-    plt.close("all")
+# for i in range(len_task):
+#     print(f'\r{i}/{len_task} - {round(i/len_task*100, 2)}%', end='')
+#     plt.axis('off')
+#     plt.imshow(X_gadf[i], extent=[0, 1, 0, 1], cmap = 'coolwarm', aspect = 'auto',vmax=abs(X_gadf[i]).max(), vmin=-abs(X_gadf[i]).max())
+#     # plt.savefig(f'{GAF_DIRECTORY}/{HAR_data_classes[labels_int[i]]}/{i}.jpg', bbox_inches='tight')
+#     plt.savefig(f'{GAF_DIRECTORY}/{i}.jpg', bbox_inches='tight')
+#     plt.close("all")
 
 
-from diffusers import DiffusionPipeline
+
+
+
+########################
+# Classification Model #
+########################
+
+# Data Preparation
+# Loading and normalizing the data.
+# Define transformations for the training and test sets
+transformations = transforms.Compose([
+    # transforms.PILToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# We define the batch size of 10
+batch_size = 2 # TODO: Must be GCD of the train-test split?
+number_of_labels = 6
+
+image_list = []
+for filename in glob.glob('diffusion_models/GAF_RGB_Images/*.jpg'): #assuming gif
+    im = Image.open(filename)
+    image_list.append(transformations(im))
+
+print(f"Total images: {len(image_list)}")
+
+# Create an instance for training. 
+# When we run this code for the first time, the CIFAR10 train dataset will be downloaded locally. 
+# train_set = CIFAR10(root="./data",train=True,transform=transformations,download=True)
+train_set = image_list[:int(0.7*len(image_list))]
+print(f"Length of train set: {len(train_set)}")
+
+# Create a loader for the training set which will read the data within batch size and put into memory.
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0)
+print("The number of images in a training set is: ", len(train_loader)*batch_size)
+
+# Create an instance for testing, note that train is set to False.
+# When we run this code for the first time, the CIFAR10 test dataset will be downloaded locally. 
+# test_set = CIFAR10(root="./data", train=False, transform=transformations, download=True)
+test_set = image_list[int(0.7*len(image_list)):]
+print(f"Length of test set: {len(test_set)}")
+
+# Create a loader for the test set which will read the data within batch size and put into memory. 
+# Note that each shuffle is set to false for the test loader.
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=0)
+print("The number of images in a test set is: ", len(test_loader)*batch_size)
+
+print("The number of batches per epoch is: ", len(train_loader))
+classes = ("Walking", "Upstairs", "Downstairs", "Sitting", "Standing", "Laying")
+
+
+# Define a convolution neural network
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=5, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(12)
+        self.conv2 = nn.Conv2d(in_channels=12, out_channels=12, kernel_size=5, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(12)
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv4 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=5, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(24)
+        self.conv5 = nn.Conv2d(in_channels=24, out_channels=24, kernel_size=5, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(24)
+        self.fc1 = nn.Linear(24*10*10, 10)
+
+    def forward(self, input):
+        output = F.relu(self.bn1(self.conv1(input)))      
+        output = F.relu(self.bn2(self.conv2(output)))     
+        output = self.pool(output)                        
+        output = F.relu(self.bn4(self.conv4(output)))     
+        output = F.relu(self.bn5(self.conv5(output)))     
+        output = output.view(-1, 24*10*10)
+        output = self.fc1(output)
+
+        return output
+
+# Instantiate a neural network model 
+model = Network()
+
+# Define the loss function with Classification Cross-Entropy loss and an optimizer with Adam optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+
+# Function to save the model
+def saveModel():
+    path = "./diffusion_models/image_classifier.pth"
+    torch.save(model.state_dict(), path)
+
+# Function to test the model with the test dataset and print the accuracy for the test images
+def testAccuracy():
+    
+    model.eval()
+    accuracy = 0.0
+    total = 0.0
+    
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            # run the model on the test set to predict labels
+            outputs = model(images)
+            # the label with the highest energy will be our prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            accuracy += (predicted == labels).sum().item()
+    
+    # compute the accuracy over all test images
+    accuracy = (100 * accuracy / total)
+    return(accuracy)
+
+
+# Training function. We simply have to loop over our data iterator and feed the inputs to the network and optimize.
+def train(num_epochs):
+    
+    best_accuracy = 0.0
+
+    # Define your execution device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("The model will be running on", device, "device")
+    # Convert model parameters and buffers to CPU or Cuda
+    model.to(device)
+
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        running_acc = 0.0
+
+        for i, (images, labels) in enumerate(train_loader, 0):
+            
+            # get the inputs
+            images = Variable(images.to(device))
+            labels = Variable(labels.to(device))
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # predict classes using images from the training set
+            outputs = model(images)
+            # compute the loss based on model output and real labels
+            loss = loss_fn(outputs, labels)
+            # backpropagate the loss
+            loss.backward()
+            # adjust parameters based on the calculated gradients
+            optimizer.step()
+
+            # Let's print statistics for every 1,000 images
+            running_loss += loss.item()     # extract the loss value
+            if i % 1000 == 999:    
+                # print every 1000 (twice per epoch) 
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 1000))
+                # zero the loss
+                running_loss = 0.0
+
+        # Compute and print the average accuracy fo this epoch when tested over all 10000 test images
+        accuracy = testAccuracy()
+        print('For epoch', epoch+1,'the test accuracy over the whole test set is %d %%' % (accuracy))
+        
+        # we want to save the model if the accuracy is the best
+        if accuracy > best_accuracy:
+            saveModel()
+            best_accuracy = accuracy
+
+# Function to show the images
+def imageshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# Function to test the model with a batch of images and show the labels predictions
+def testBatch():
+    # get batch of images from the test DataLoader  
+    images, labels = next(iter(test_loader))
+
+    # show all images as one image grid
+    # imageshow(torchvision.utils.make_grid(images))
+   
+    # Show the real labels on the screen 
+    print('Real labels: ', ' '.join('%5s' % classes[labels[j]] 
+                               for j in range(batch_size)))
+  
+    # Let's see what if the model identifiers the  labels of those example
+    outputs = model(images)
+    
+    # We got the probability for every 10 labels. The highest (max) probability should be correct label
+    _, predicted = torch.max(outputs, 1)
+    
+    # Let's show the predicted labels on the screen to compare with the real ones
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] 
+                              for j in range(batch_size)))
+
+
+# Let's build our model
+train(5)
+print('Finished Training')
+
+# Test which classes performed well
+testAccuracy()
+
+# Let's load the model we just created and test the accuracy per label
+model = Network()
+path = "diffusion_models/image_classifier.pth"
+model.load_state_dict(torch.load(path))
+
+# Test with batch of images
+testBatch()
+
+
+
+# from diffusers import DiffusionPipeline
 
 # Unconditional diffusion model
-pipeline = DiffusionPipeline.from_pretrained("google/ddpm-celebahq-256")
+# pipeline = DiffusionPipeline.from_pretrained("google/ddpm-celebahq-256")
 
 # test_img = pipeline(
 #     train_data_dir="classification_images_gadf",
