8f359a4c345d8c2f7da541515360fe7577f70415
jasondykstra <jasondykstra2000@gmail.com>
2023-03-04 08:00:28 -0800

    added model evaluation code

--- diffusion_models/GAF_Classification.py
+++ diffusion_models/GAF_Classification.py
@@ -23,6 +23,8 @@ from torch.utils.data import Dataset
 from datasets import load_dataset
 import pathlib
 
+TRAIN = True
+
 
 if torch.cuda.is_available():
     device = 'cuda'
@@ -121,271 +123,109 @@ class CNNClassifier(nn.Module):
 # Create the Discriminator
 cnn = CNNClassifier().to(device)
 
-# Apply the weights_init function to randomly initialize all weights
-#  to mean=0, stdev=0.2.
-cnn.apply(weights_init)
+if TRAIN:
+    # Apply the weights_init function to randomly initialize all weights
+    #  to mean=0, stdev=0.2.
+    cnn.apply(weights_init)
 
-# Print the model
-# print(cnn)
-
-
-# Initialize BCELoss function
-criterion = nn.MSELoss()
-
-# Setup Adam optimizers for both G and D
-# optimizerD = optim.AdamW(netD.parameters(), lr=lr)
-optimizerD = optim.Adam(cnn.parameters(), lr=lr, betas=(beta1, 0.999))
+    # Print the model
+    # print(cnn)
 
 
-# Training Loop
+    # Initialize BCELoss function
+    criterion = nn.MSELoss()
 
-# Lists to keep track of progress
-D_losses = []
-iters = 0
+    # Setup Adam optimizers for both G and D
+    # optimizerD = optim.AdamW(netD.parameters(), lr=lr)
+    optimizerD = optim.Adam(cnn.parameters(), lr=lr, betas=(beta1, 0.999))
 
-# for i, (data_x, data_y) in enumerate(train_loader, 0):
-#     X = data_x.to(device)
-#     Y = data_y.to(device)
-#     print(X.shape)
-#     print(Y.shape)
 
-print("Starting Training Loop...")
-# For each epoch
-for epoch in range(num_epochs):
-    # For each batch in the dataloader
-    for i, (data_x, data_y) in enumerate(train_loader, 0):
-        
-        
-        X = data_x.to(device)
-        Y = data_y.to(device)
+    # Training Loop
 
-        cnn.zero_grad()
-        output = cnn(X)
-        loss = criterion(output.float(), Y.float())
-        loss.backward()
-        optimizerD.step()
-        
+    # Lists to keep track of progress
+    D_losses = []
+    iters = 0
 
-        # Output training stats
-        if i % 400 == 0:
-            print(f'Epoch {epoch}/{num_epochs}')
-            print(f'MSE for Train: {loss}')
+    # for i, (data_x, data_y) in enumerate(train_loader, 0):
+    #     X = data_x.to(device)
+    #     Y = data_y.to(device)
+    #     print(X.shape)
+    #     print(Y.shape)
+
+    print("Starting Training Loop...")
+    # For each epoch
+    for epoch in range(num_epochs):
+        # For each batch in the dataloader
+        for i, (data_x, data_y) in enumerate(train_loader, 0):
             
-            val_preds = []
-            val_labels = []
-            for _, (val_x, val_y) in enumerate(test_loader, 0):
-                val_X = val_x.to(device)
-                val_Y = val_y.to(device)
-                with torch.no_grad():
-                    val_output = cnn(val_X)
-                
-                val_preds.append(val_output)
-                val_labels.append(val_Y)
-                
-            val_preds = torch.concat(val_preds)
-            val_labels =  torch.concat(val_labels)
             
-            val_loss = criterion(val_preds, val_labels)
-            print(f'Test MSE: {val_loss}')
-                
-                
+            X = data_x.to(device)
+            Y = data_y.to(device)
+
+            cnn.zero_grad()
+            output = cnn(X)
+            loss = criterion(output.float(), Y.float())
+            loss.backward()
+            optimizerD.step()
             
-            print()
 
-        # Save Losses for plotting later
-        D_losses.append(loss.item())
-  
-
-        iters += 1
-
-
-
-exit()
-
-
-class cifar10(Dataset):
-	def __init__(self, root, train = False, transforms = None):
-
-		self.root = root
-		self.transforms = transforms
-		self.train = train
-
-
-		self.train_data = [file for file in os.listdir(root) if "data_batch" in file] # TODO make test and train
-		self.test_data = [file for file in os.listdir(root) if "test_batch" in file]
-
-		self.data_files = self.train_data if self.train else self.test_data
-		self.images = []
-		self.labels = []
-		
-		self.load_data()
-
-
-	def __len__(self):
-		return len(self.images)
-
-	def __getitem__(self, idx):
-
-		if torch.is_tensor(idx):
-			idx = idx.tolist()
-
-		image = self.images[idx]
-		image = Image.fromarray(image)
-
-		label = self.labels[idx]
-
-		if self.transforms:
-			image = self.transforms(image)
-
-		return image, label
-
-
-	def load_data(self):
-
-		for file in self.data_files:
-			file_path = os.path.join(self.root, file)
-			sample = self.read_file(file_path)
-			self.images.append(sample["data"])
-			self.labels.extend(sample["labels"])
-
-
-		self.images = np.vstack(self.images).reshape(-1, 3, 32, 32)
-		self.images = self.images.transpose((0, 2, 3, 1))
-
-	def read_file(self, filename):
-		with open(filename, "rb") as f:
-			f = pickle.load(f, encoding = "latin1")
-		return f
-
-
-
-
-
-transformations = transforms.Compose([
-    # transforms.PILToTensor()
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
-
-# image_list = []
-# for filename in glob.glob('diffusion_models/GAF_RGB_Images/*.jpg'):
-#     im = Image.open(filename)
-#     image_list.append(transformations(im))
-
-# print(f"Total images: {len(image_list)}")
-
-# # Create an instance for training. 
-# # When we run this code for the first time, the CIFAR10 train dataset will be downloaded locally. 
-# # train_set = CIFAR10(root="./data",train=True,transform=transformations,download=True)
-# train_set = image_list[:int(0.7*len(image_list))]
-# print(f"Length of train set: {len(train_set)}")
-
-
-# if __name__ == "__main__":
-        
-#     import torch
-#     import torchvision
-#     import torchvision.transforms as transforms
-
-
-#     transform = transforms.Compose(
-#         [transforms.ToTensor(),
-#         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-#     batch_size = 4
-
-#     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-#                                             download=True, transform=transform)
-#     print(trainset)
-#     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-#                                             shuffle=True, num_workers=2)
-
-#     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-#                                         download=True, transform=transform)
-#     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-#                                             shuffle=False, num_workers=2)
-
-#     classes = ('plane', 'car', 'bird', 'cat',
-#             'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
-
-#     import torch.nn as nn
-#     import torch.nn.functional as F
-
-
-#     class Net(nn.Module):
-#         def __init__(self):
-#             super().__init__()
-#             self.conv1 = nn.Conv2d(3, 6, 5)
-#             self.pool = nn.MaxPool2d(2, 2)
-#             self.conv2 = nn.Conv2d(6, 16, 5)
-#             self.fc1 = nn.Linear(16 * 5 * 5, 120)
-#             self.fc2 = nn.Linear(120, 84)
-#             self.fc3 = nn.Linear(84, 10)
-
-#         def forward(self, x):
-#             x = self.pool(F.relu(self.conv1(x)))
-#             x = self.pool(F.relu(self.conv2(x)))
-#             x = torch.flatten(x, 1) # flatten all dimensions except batch
-#             x = F.relu(self.fc1(x))
-#             x = F.relu(self.fc2(x))
-#             x = self.fc3(x)
-#             return x
-
-
-#     net = Net()
-
-
-
-#     import torch.optim as optim
-
-#     criterion = nn.CrossEntropyLoss()
-#     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-
-
-#     for epoch in range(1):  # loop over the dataset multiple times
-
-#         running_loss = 0.0
-#         for i, data in enumerate(trainloader, 0):
-#             # get the inputs; data is a list of [inputs, labels]
-#             inputs, labels = data
-
-#             # zero the parameter gradients
-#             optimizer.zero_grad()
-
-#             # forward + backward + optimize
-#             outputs = net(inputs)
-#             loss = criterion(outputs, labels)
-#             loss.backward()
-#             optimizer.step()
-
-#             # print statistics
-#             running_loss += loss.item()
-#             if i % 2000 == 1999:    # print every 2000 mini-batches
-#                 print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-#                 running_loss = 0.0
-
-#     print('Finished Training')
-
-
-
-#     dataiter = iter(testloader)
-#     images, labels = next(dataiter)
-
-
-#     correct = 0
-#     total = 0
-#     # since we're not training, we don't need to calculate the gradients for our outputs
-#     with torch.no_grad():
-#         for data in testloader:
-#             images, labels = data
-#             # calculate outputs by running images through the network
-#             outputs = net(images)
-#             # the class with the highest energy is what we choose as prediction
-#             _, predicted = torch.max(outputs.data, 1)
-#             total += labels.size(0)
-#             correct += (predicted == labels).sum().item()
-
-#     print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+            # Output training stats
+            if i % 400 == 0:
+                print(f'Epoch {epoch}/{num_epochs}')
+                print(f'MSE for Train: {loss}')
+                
+                val_preds = []
+                val_labels = []
+                for _, (val_x, val_y) in enumerate(test_loader, 0):
+                    val_X = val_x.to(device)
+                    val_Y = val_y.to(device)
+                    with torch.no_grad():
+                        val_output = cnn(val_X)
+                    
+                    val_preds.append(val_output)
+                    val_labels.append(val_Y)
+                    
+                val_preds = torch.concat(val_preds)
+                val_labels =  torch.concat(val_labels)
+                
+                val_loss = criterion(val_preds, val_labels)
+                print(f'Test MSE: {val_loss}')
+                    
+                    
+                
+                print()
+
+            # Save Losses for plotting later
+            D_losses.append(loss.item())
+    
+
+            iters += 1
+
+    torch.save(cnn.state_dict(), "diffusion_models/classifiers/GAF_Classifier.pth")
+
+else:
+    cnn.load_state_dict(torch.load("diffusion_models/classifiers/GAF_Classifier.pth"))
+
+def check_accuracy(test_loader: DataLoader, model: nn.Module, device):
+    num_correct = 0
+    total = 0
+    model.eval()
+
+    with torch.no_grad():
+        for data, labels in test_loader:
+            data = data.to(device=device)
+            labels = labels.to(device=device)
+            print(f"Looking at: {labels}")
+
+            predictions = model(data)
+            print(f"Model predicted: {predictions}")
+            num_correct += (predictions == labels).sum()
+            total += labels.size(0)
+
+        print(f"Test Accuracy of the model: {float(num_correct)/float(total)*100:.2f}")
+
+check_accuracy(test_loader, cnn, "cpu")
+
+# model = TheModelClass(*args, **kwargs)
+# model.load_state_dict(torch.load(PATH))
+# model.eval()
