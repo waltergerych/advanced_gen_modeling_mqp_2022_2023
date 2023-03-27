e079ff212b223db90de3c96681bff58d04c280ee
cindytrac <cindy.trac.11@gmail.com>
2023-03-27 00:19:39 -0400

    Update GAF_Classification.py
    gaf cross entropy no softmax

--- diffusion_models/GAF_Classification.py
+++ diffusion_models/GAF_Classification.py
@@ -20,7 +20,7 @@ from PIL import Image
 import os
 import pickle
 from torch.utils.data import Dataset
-from datasets import load_dataset
+#from datasets import load_dataset
 import pathlib
 
 TRAIN = True
@@ -48,6 +48,7 @@ num_epochs = 5
 lr = 0.002
 
 # Beta1 hyperparam for Adam optimizers
+# why 0.5???
 beta1 = 0.5
 
 
@@ -60,25 +61,40 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
-
+# we're working on this rn
 train_path = "diffusion_models/GAF_Classification/train"
 test_path = "diffusion_models/GAF_Classification/test"
 
+# this was for when the GAFs were NOT squares
+# train_transforms = transforms.Compose([
+#     transforms.ToTensor(),
+#     transforms.Resize(32, interpolation=torchvision.transforms.functional.InterpolationMode.BILINEAR),
+#     transforms.CenterCrop(32),
+#     # transforms.RandomHorizontalFlip(p=0.5)
+#     ])
+
+# test_transforms = transforms.Compose([
+#     transforms.ToTensor(),
+#     transforms.Resize(32, interpolation=torchvision.transforms.functional.InterpolationMode.BILINEAR),
+#     transforms.CenterCrop(32),
+#     ])
+
 train_transforms = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Resize(32, interpolation=torchvision.transforms.functional.InterpolationMode.BILINEAR),
-    transforms.CenterCrop(32),
-    # transforms.RandomHorizontalFlip(p=0.5)
+    
     ])
 
 test_transforms = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Resize(32, interpolation=torchvision.transforms.functional.InterpolationMode.BILINEAR),
-    transforms.CenterCrop(32),
+    
     ])
+
 
 train_loader = DataLoader(torchvision.datasets.ImageFolder(train_path, transform=train_transforms), batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(torchvision.datasets.ImageFolder(test_path, transform=test_transforms), batch_size=batch_size, shuffle=True)
+
+# train_loader = DataLoader(torchvision.datasets.ImageFolder(train_path), batch_size=batch_size, shuffle=True)
+# test_loader = DataLoader(torchvision.datasets.ImageFolder(test_path), batch_size=batch_size, shuffle=True)
 
 # Get class names
 root = pathlib.Path(train_path)
@@ -106,17 +122,32 @@ class CNNClassifier(nn.Module):
             nn.Dropout(0.2),
             nn.Flatten()
         )
-        self.lin1 = nn.Linear(512, 256)
+        self.lin1 = nn.Linear(98304, 256)
         self.relu = nn.LeakyReLU(0.2)
-        self.lin2 = nn.Linear(256,1)
-        self.sig = nn.Sigmoid()
+        self.lin2 = nn.Linear(256,6)
+        # self.sig = nn.Sigmoid()
+
+        # don't have to add in another activation function because linear is same shape as output of softmax
+        # self.softmax = torch.nn.Softmax(dim=1)
+
+        
+        # want loss function that assumes normalization 
         
 
     def forward(self, input):
+        print(input.shape)
         y = self.main(input)
-        y = self.relu(self.lin1(y))
+        print(y.shape)
+        y = self.lin1(y)
+        print(y.shape)
+        y = self.relu(y)
+        print(y.shape)
         y = self.lin2(y)
-        y = self.sig(y).squeeze(1)
+        print("final output: ", y.shape)
+        # y = self.softmax(y)
+        # print(y.shape)
+        # y = self.sig(y).squeeze(1)
+
         return y
 
 
@@ -133,7 +164,10 @@ if TRAIN:
 
 
     # Initialize BCELoss function
-    criterion = nn.MSELoss()
+    # criterion = nn.MSELoss()
+
+    criterion = nn.CrossEntropyLoss()
+    
 
     # Setup Adam optimizers for both G and D
     # optimizerD = optim.AdamW(netD.parameters(), lr=lr)
@@ -158,13 +192,16 @@ if TRAIN:
         # For each batch in the dataloader
         for i, (data_x, data_y) in enumerate(train_loader, 0):
             
-            
-            X = data_x.to(device)
-            Y = data_y.to(device)
+            # only need if running on turing
+            # X = data_x.to(device)
+            # Y = data_y.to(device)
 
-            cnn.zero_grad()
-            output = cnn(X)
-            loss = criterion(output.float(), Y.float())
+            optimizerD.zero_grad()
+            output = cnn(data_x)
+            # convert output to their respective classes
+            
+            # pred = torch.add(torch.argmax(output), 1)
+            loss = criterion(output, data_y)
             loss.backward()
             optimizerD.step()
             
@@ -172,24 +209,25 @@ if TRAIN:
             # Output training stats
             if i % 400 == 0:
                 print(f'Epoch {epoch}/{num_epochs}')
-                print(f'MSE for Train: {loss}')
+                print(f'Train Cross-Entropy Loss: {loss}')
                 
                 val_preds = []
                 val_labels = []
                 for _, (val_x, val_y) in enumerate(test_loader, 0):
-                    val_X = val_x.to(device)
-                    val_Y = val_y.to(device)
+                    #val_X = val_x.to(device)
+                    #val_Y = val_y.to(device)
                     with torch.no_grad():
-                        val_output = cnn(val_X)
+                        val_output = cnn(val_x)
+                        # val_pred = np.argmax(val_output) + 1
                     
                     val_preds.append(val_output)
-                    val_labels.append(val_Y)
+                    val_labels.append(val_y)
                     
                 val_preds = torch.concat(val_preds)
                 val_labels =  torch.concat(val_labels)
                 
                 val_loss = criterion(val_preds, val_labels)
-                print(f'Test MSE: {val_loss}')
+                print(f'Test Cross-Entropy Loss: {val_loss}')
                     
                     
                 
@@ -201,10 +239,10 @@ if TRAIN:
 
             iters += 1
 
-    torch.save(cnn.state_dict(), "diffusion_models/classifiers/GAF_Classifier.pth")
+    torch.save(cnn.state_dict(), "diffusion_models/classifiers/GAF_Classifier_CrossEntropy.pth")
 
 else:
-    cnn.load_state_dict(torch.load("diffusion_models/classifiers/GAF_Classifier.pth"))
+    cnn.load_state_dict(torch.load("diffusion_models/classifiers/GAF_Classifier_CrossEntropy.pth"))
 
 def check_accuracy(test_loader: DataLoader, model: nn.Module, device):
     num_correct = 0
